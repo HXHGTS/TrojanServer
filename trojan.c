40288fdbde6ee56c4a10387b99321f7509adc44a
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-09-01 17:21:13 +0800

    Add files via upload

--- trojan.c
+++ trojan.c
@@ -8,7 +8,8 @@ int mode;
 
 int main(){
     system("clear");
-    Menu:UI();
+Menu:UI();
+    system("clear");
     if (mode == 1) {
         install_trojan();
         goto Menu;
@@ -19,16 +20,6 @@ int main(){
         goto Menu;
     }
     else if (mode == 3) {
-        system("cat /usr/local/etc/trojan/config.json");
-        goto Menu;
-    }
-    else if (mode == 4) {
-        system("systemctl stop trojan");
-        system("sudo vi /usr/local/etc/trojan/config.json");
-        system("systemctl start trojan");
-        goto Menu;
-    }
-    else if (mode == 5) {
         printf("手机trojan客户端请扫描二维码添加:\n\n");
         system("qrencode -t ansiutf8 < /usr/local/etc/trojan/client.conf");
         system("sleep 5");
@@ -37,7 +28,21 @@ int main(){
         printf("\n\n");
         goto Menu;
     }
+    else if (mode == 4) {
+        system("cat /usr/local/etc/trojan/config.json");
+        goto Menu;
+    }
+    else if (mode == 5) {
+        system("systemctl stop trojan");
+        system("sudo vi /usr/local/etc/trojan/config.json");
+        system("systemctl start trojan");
+        goto Menu;
+    }
     else if (mode == 6) {
+        DNS_Change();
+        goto Menu;
+    }
+    else if (mode == 7) {
         system("systemctl stop trojan");
         goto Menu;
     }
@@ -53,7 +58,10 @@ int UI() {
     printf("-----------------------------------------------------------\n");
     printf("安装前请先去阿里云或腾讯云申请域名与域名对应的SSL证书，否则会导致安装失败！\n");
     printf("-----------------------------------------------------------\n");
-    printf("1.安装trojan\n2.运行trojan\n3.查看服务器配置\n4.修改服务器配置\n5.显示二维码与配置链接\n6.关闭trojan\n0.退出\n");
+    printf("----------------------当前Kernel版本-----------------------\n");
+    system("uname -sr");
+    printf("-----------------------------------------------------------\n");
+    printf("1.安装trojan\n2.运行trojan\n3.显示二维码与配置链接\n4.查看服务器配置\n5.修改服务器配置\n6.修改DNS服务器\n7.关闭trojan\n0.退出\n");
     printf("-----------------------------------------------------------\n");
     printf("请输入:");
     scanf("%d", &mode);
@@ -63,7 +71,7 @@ int UI() {
 int install_trojan() {
     KernelUpdate();
     system("setenforce 0");
-    system("yum install -y curl pwgen qrencode unzip epel-release nginx firewalld");
+    system("yum install -y curl pwgen qrencode unzip epel-release nginx");
     system("wget https://raw.githubusercontent.com/trojan-gfw/trojan-quickstart/master/trojan-quickstart.sh -O /root/trojan-quickstart.sh");
     system("chmod +x trojan-quickstart.sh");
     system("bash trojan-quickstart.sh");
@@ -88,11 +96,6 @@ int install_trojan() {
     fprintf(config, "        \"%s\"\n", passwd);
     fclose(config);
     system("curl https://raw.githubusercontent.com/HXHGTS/TrojanServer/master/trojan.conf.2 >> /usr/local/etc/trojan/config.json");
-    printf("正在配置防火墙. . .\n");
-    system("systemctl enable firewalld && systemctl start firewalld");
-    system("firewall-cmd --permanent --add-service=https");
-    system("firewall-cmd --permanent --add-service=http");
-    system("firewall-cmd --reload");
     printf("正在配置html网页. . .\n");
     system("curl https://raw.githubusercontent.com/HXHGTS/TrojanServer/master/html.zip > /usr/share/nginx/html/html.zip");
     system("unzip -o /usr/share/nginx/html/html.zip -d /usr/share/nginx/html");
@@ -102,6 +105,13 @@ int install_trojan() {
     config = fopen("/usr/local/etc/trojan/client.conf", "w");
     fprintf(config, "trojan://%s@%s:443", passwd,sni);
     fclose(config);
+    printf("正在配置防火墙. . .\n");
+    system("yum install -y firewalld");
+    system("ufw disable");
+    system("systemctl enable firewalld && systemctl start firewalld");
+    system("firewall-cmd --permanent --add-service=https");
+    system("firewall-cmd --permanent --add-service=http");
+    system("firewall-cmd --reload");
     printf("正在启动trojan并将trojan写入开机引导项. . .\n");
     system("systemctl enable trojan && systemctl start trojan");
     printf("正在验证trojan启动，不为空则启动成功. . .\n");
@@ -129,5 +139,44 @@ int KernelUpdate() {
         system("chmod +x TCPO.sh");
         system("bash TCPO.sh");
     }
+    return 0;
+}
+
+int DNS_Change() {
+    int dns;
+    char ns1[16], ns2[16],command[50];
+    printf("请选择想要设定的DNS:\n\n1.Google\n\n2.Cloudflare\n\n3.Opendns\n\n4.Quad9\n\n5.Level3\n\n请输入:");
+    scanf("%d", &dns);
+    if (dns == 1) {
+        sprintf(ns1, "8.8.8.8");
+        sprintf(ns2, "8.8.4.4");
+    }
+    else if (dns == 2) {
+        sprintf(ns1, "1.1.1.1");
+        sprintf(ns2, "1.0.0.1");
+    }
+    else if (dns == 3) {
+        sprintf(ns1, "208.67.222.222");
+        sprintf(ns2, "208.67.220.220");
+    }
+    else if (dns == 4) {
+        sprintf(ns1, "9.9.9.9");
+        sprintf(ns2, "149.112.112.9");
+    }
+    else {
+        sprintf(ns1, "4.2.2.1");
+        sprintf(ns2, "4.2.2.2");
+    }
+    sprintf(command, "echo \"nameserver %s\" > /etc/resolv.conf", ns1);
+    system(command);
+    sprintf(command, "echo \"nameserver %s\" >> /etc/resolv.conf", ns2);
+    system(command);
+    system("sed -e '/DNS1=/d;/DNS2=/d;/DNS3=/d' /etc/sysconfig/network-scripts/ifcfg-eth0");
+    sprintf(command, "echo \"DNS1=%s\" >> /etc/sysconfig/network-scripts/ifcfg-eth0", ns1);
+    system(command);
+    sprintf(command, "echo \"DNS2=%s\" >> /etc/sysconfig/network-scripts/ifcfg-eth0", ns2);
+    system(command);
+    system("service network restart");
+    printf("DNS服务器修改成功!\n");
     return 0;
 }
